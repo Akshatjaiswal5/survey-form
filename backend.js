b034c851bb84aae1b6b428c027184912db03970d
DESKTOP-T526RFM\lenovo <akshatjaiswal521@gmail.com>
2021-10-18 02:23:26 +0530

    added nodejs backend

---
+++ backend.js
@@ -0,0 +1,27 @@
+var express = require('express');
+const path = require('path');
+var app = express();
+app.use(express.json());
+
+app.get('/', function(req, res) {
+   res.sendFile(path.join(__dirname, '/index.html'));
+ });
+
+app.get('/index.css', function(req, res) {
+   res.sendFile(path.join(__dirname, '/index.css'));
+ });
+
+app.get('/index.js', function(req, res) {
+   res.sendFile(path.join(__dirname, '/index.js'));
+ });
+
+app.post('/', function(req, res) {
+
+ });
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-var express = require('express');
-var app = express();
+function run()
+{
+ event.preventDefault();
+ const name=document.getElementById('name').value;
+ const email=document.getElementById('email').value;
+ const number=document.getElementById('number').value;
+ const branch=document.getElementById('dropdown').value;
+ const batch=document.querySelector('input[name="batch"]:checked').value;;
+ const diplomaStudent=document.getElementById('diploma-student').checked;
+ const hostelite=document.getElementById('hostelite').checked;
+ const immigrant=document.getElementById('out-of-state-immigrant').checked;
+ const feedback=document.getElementById('feedback').value;
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+ var data={
+  'name':name,
+  'email':email,
+  'number':number,
+  'branch': branch,
+  'batch': batch,
+  'diplomaStudent': diplomaStudent,
+  'hostelite': hostelite,
+  'immigrant': immigrant,
+  'feedback': feedback,
+ }
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+ fetch(location.href, {
+  method: 'POST',
+  headers:{},
+  body:data
+ })
+ .catch
+ {
+  alert("Network error");
+ }
+
+}
